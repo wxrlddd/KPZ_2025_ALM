83f9e1957b9630dc12577d81a4ce9ef2575460e1
wxrld <ipz232_alm@student.ztu.edu.ua>
2025-05-28 23:59:29 +0300

    Task4: додано Image з патерном Стратегія

---
+++ Lab 3/Lab3.Composite/Lab3.Composite/LightImageNode.cs
@@ -0,0 +1,34 @@
+﻿using Lab3.Composite.LoadStrategies;
+
+namespace Lab3.Composite
+{
+    public class LightImageNode : LightNode
+    {
+        private readonly string _href;
+        private readonly ILoadStrategy _strategy;
+
+        public LightImageNode(string href)
+        {
+            _href = href;
+            // вибір стратегії залежно від префіксу
+            if (href.StartsWith("http://") || href.StartsWith("https://"))
+                _strategy = new WebLoadStrategy();
+            else
+                _strategy = new FileLoadStrategy();
+        }
+
+        public override string InnerHTML() => string.Empty;
+
+        public override string OuterHTML()
+        {
+            // self-closing тег img з атрибутом src
+            return $"<img src=\"{_href}\" />";
+        }
+
+        
+        public void Load()
+        {
+            _strategy.Load(_href);
+        }
+    }
+}
